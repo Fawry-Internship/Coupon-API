3e2e895b37d87394fd60756d87e6788437a5a73b
sameh-tarek <tarksame7@gmail.com>
2024-04-14 22:46:07 +0200

    add cros origin

--- src/main/java/com/intern/CoponAPI/resource/ConsumptionResource.java
+++ src/main/java/com/intern/CoponAPI/resource/ConsumptionResource.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("coupon")
